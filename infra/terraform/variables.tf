8ffeb402dba80afe27a2c5c34dd8b948fe92e959
Eunice js <90851478+Eunice2000@users.noreply.github.com>
2026-02-08 17:41:32 +0100

    Add duplicate aws_region variable definition
    Added duplicate aws_region variable definition.

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -3,6 +3,12 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
 
 variable "project_name" {
   description = "Project name used for tagging and naming"
