bcd7cb5138592b98bb82f1ef056604062028975c
Eunice js <90851478+Eunice2000@users.noreply.github.com>
2026-02-08 19:33:27 +0100

    Update config.py

--- app/src/config.py
+++ app/src/config.py
@@ -11,7 +11,7 @@ class Development:
     DEBUG = True
     TESTING = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
 
 
 class Production:
@@ -21,7 +21,7 @@ class Production:
     DEBUG = False
     TESTING = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
 
 
 app_config = {
