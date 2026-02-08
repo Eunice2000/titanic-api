46fd2e9ff654b91be0289bb62fbe9fa2ac01de88
Eunice js <90851478+Eunice2000@users.noreply.github.com>
2026-02-08 17:36:28 +0100

    Remove Helm provider configuration from main.tf
    Removed Helm provider configuration for EKS cluster.

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -430,17 +430,6 @@ resource "aws_eks_access_policy_association" "admin_user_cluster_admin" {
     type = "cluster"
   }
 }
-provider "helm" {
-  kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-      command     = "aws"
-    }
-  }
-}
 
 resource "helm_release" "external_secrets" {
   name       = "external-secrets"
